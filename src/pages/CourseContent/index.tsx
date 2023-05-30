928ecd81aa9f2d04929f6b90bd7df91a264f5ca4
JureRelja <jurereljanovic99@gmail.com>
2023-05-30 21:17:44 +0200

    second commit

--- src/pages/CourseContent/index.tsx
+++ src/pages/CourseContent/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { coursesEng, coursesHr } from "../../text/courses/Courses";
 import useText from "../../hooks/textLanguage";
